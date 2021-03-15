634e94a81d901211083286c004c5345a01c777a8
b05902105 <b05902105@ntu.eud.tw>
2021-03-16 05:52:55 +0800

    modify to my simpler version

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PREFIX="/bin"
-
-cp bin/rm.sh $PREFIX/safe-rm
-chmod 755 $PREFIX/safe-rm
-echo "Installation Succeeded!"
-echo "Please add \"alias rm='$PREFIX/safe-rm'\" to your ~/.bashrc script"
-echo "Enjoy!"
--- uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PREFIX="/bin"
-
-rm $PREFIX/safe-rm
-echo "Please remove \"alias rm='$PREFIX/safe-rm'\" from your ~/.bashrc script"
-echo "and do 'unalias rm' from all your terminal sessions"
-echo "Successfully removed $PREFIX/safe-rm"
